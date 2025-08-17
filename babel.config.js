09af26707e12e20eda45e94b0527ecfed823ea5b
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2025-08-17 12:42:26 +0600

    Modification: Modified package.json file.

--- babel.config.js
+++ babel.config.js
@@ -2,34 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins: [
-    //   [
-    //     'module-resolver',
-    //     {
-    //       root: ['./'],
-    //       alias: {
-    //         '@': './',
-    //       },
-    //     },
-    //   ],
-    // ],
+    plugins: [
+      // Enable @ alias imports like `import x from '@/utils/x'`
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        },
+      ],
+      // Expo Router plugin is required when using expo-router/entry
+      'expo-router/babel',
+    ],
   };
 };
-
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: ['babel-preset-expo'],
-//     // plugins: [
-//     //   [
-//     //     'babel-plugin-module-resolver', // <-- Full name
-//     //     {
-//     //       root: ['./'],
-//     //       alias: {
-//     //         '@': './',
-//     //       },
-//     //     },
-//     //   ],
-//     // ],
-//   };
-// };
